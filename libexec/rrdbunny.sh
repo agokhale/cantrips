37c8b6cf19bbfdd10dfb94917d902b4e02728359
agokhale <ash@aeria.net>
2017-11-22 14:52:06 -0500

    usage + options

--- libexec/rrdbunny.sh
+++ libexec/rrdbunny.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/sh 
+
+
 # show rrd data in 
 # $2...n  is the data source type
 # $1 is the filename
@@ -18,24 +20,47 @@ colorburst()
 }
 
 if [ $# -lt 1 ] ; then
-	echo "args: "
-	echo  " graph output rrd_bunny.sh a.file.rrd "
-	echo " get valid dataset choices: rrd_bunny.sh "
+	cat  << asdf
+	rrdbunny.sh graph output: rrdbunny.sh a.file*.rrd
+
+	knobs for turning are provided by getenv: 
+
+setenv RRDBUNNY_RESOLUTION " --width 3800 --height 1600 "
+
+#shrink the view window by some hours
+setenv RRDBUNNY_SHRINK_START 0
+setnv RRDBUNNY_SHRINK_STOP  0
+#desination override
+setenv RRDBUNNY_OUTPIPE  " xv - " 
+#set timescale
+setenv  RRDBUNNY_TIMESCALE  6
+
+##
+#ra_idx=0 #3 hours
+#ra_idx=1 #3 hours
+#ra_idx=2 #3  hours
+#ra_idx=3 #24 hours
+#ra_idx=4 #24 hours
+#ra_idx=5 #24 hours
+#ra_idx=6 #7*(24) hours
+#ra_idx=7 #7*(24) hours
+#ra_idx=8 #7*(24) hours
+#ra_idx=9  # 1 month 
+#ra_idx=10 # 1 month 
+#ra_idx=11 # 1 month 
+#ra_idx=12 # 1 year 
+
+	
+asdf
 	exit
 fi 
 
-if [ $# -lt 2 ] ; then
-	echo  "$1 available fields:"
-	rrdtool info $1 | grep 'ds.*index'
-	rrdtool info $1 | grep '^ds.*index' | sed 's/ds\[\(.*\)\].*/\1/' | xargs rrdbunny.sh $1
-	exit
-fi
 
 infiles=$@
 
+ra_index=${RRDBUNNY_TIMESCALE:-"6"}
+export RRDBUNNY_TIMESCALE
 
-
-ra_index=6 
 rrd_infile=$1
 #get epoch timestamps
 first_ts=`rrdtool first --rraindex $ra_index $rrd_infile`
@@ -44,12 +69,19 @@ last_ts=`rrdtool last $rrd_infile`
 echo "view window from$first_ts `date -r $first_ts` to: $last_ts `date -r $last_ts`"
 
 #shrink the view window by some hours
-#first_ts=$((  $first_ts + (3600 * (0) )    ))
-#last_ts=$((    $last_ts - (3600 * (0) )       ))
+shrink_start=${RRDBUNNY_SHRINK_START:-"0"}
+shrink_stop=${RRDBUNNY_SHRINK_STOP:-"0"}
+export RRDBUNNY_SHRINK_STOP
+export RRDBUNNY_SHRINK_START
+first_ts=$((  $first_ts + (3600 * (shrink_start) )    ))
+last_ts=$((    $last_ts - (3600 * (shrink_stop ) )    ))
 
 resolution=${RRDBUNNY_RESOLUTION:-" --width 800 --height 600 "}
+options=${RRDBUNNY_OPTIONS:-" --grid-dash 1:3  "}
+export RRBDUNNY_RESOLUTION
 echo $resolution
 output_pipeline=${RRDBUNNY_OUTPIPE:-" xv - "}
+export RRDBUNNY_OUTPIPE
 echo $output_pipeline
 echo 'control q to quit xv'
 
@@ -72,10 +104,10 @@ for file_cursor in $infiles; do
 		legend_min="$legend_text-min"
 		defs=" DEF:bunny$uniqueds-max=$file_cursor:$ds_cursor:MAX $defs "
 		defs=" DEF:bunny$uniqueds-min=$file_cursor:$ds_cursor:MIN $defs "
-		lines=" LINE1:bunny$uniqueds-max#$color:'$legend_max' $lines " 
+		lines=" LINE1:bunny$uniqueds-max#$color:'$legend_max':STACK $lines " 
 		colorburst $((uniqueds+1))
-		lines=" LINE1:bunny$uniqueds-min#$color:'$legend_min' $lines " 
-		#lines=" LINE1:bunny$uniqueds-min#$color:'nnies/femptofornight' $lines " 
+		lines=" LINE1:bunny$uniqueds-min#$color:'$legend_min':dashes $lines " 
+		#lines=" AREA:bunny$uniqueds-min#$color:'$legend_min' $lines " 
 		#LINE1:bunny67-min#219CCC:'bnnydelete-max'
 
 		dsnumber=$((dsnumber + 1))
@@ -96,7 +128,7 @@ rrdtool graph - --imgformat PNG \
 	--end  $last_ts \
 	$lines       \
 	--color CANVAS#000000 --color BACK#000000 --color FONT#FFFFFF \
-	$resolution        \
+	$resolution  $options \
 	| $output_pipeline
 echo fin 
 exit 0
