6cc6b4a34740e8f90a3f8cce36d43da20b244763
agokhale <ash@aeria.net>
2019-06-05 13:53:22 -0400

    let smart psudorando

--- libexec/psurando.sh
+++ libexec/psurando.sh
@@ -2,14 +2,15 @@
 # fast psurando generated file, much  faster than /dev/urandom 
 # psurando.sh /out/path 1(g)
 
-tpath=$1
-gigs=$2
+gigs=$1
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
+	echo  "psudorando.sh <sizeinGiB>"
+	echo  "psurando.sh 30 | dpv 30000024000:wat -o /dev/null"
         exit -4
 fi
 echo writing psurandom $tpath with $gigs G of payload
 sleep 0.3
-dd if=/dev/zero bs=1G count=$gigs | openssl enc -aes-128-cbc -k swordfiiish > $tpath
+dd if=/dev/zero bs=1G count=$gigs | openssl enc -aes-128-cbc -k swordfiiish 
 
 
