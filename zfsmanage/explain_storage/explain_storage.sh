e1a993563bbf5801ee6911fce473c58497a0eea0
Charlie Root <you@example.com>
2021-03-15 16:29:59 +0000

    usable

--- zfsmanage/explain_storage/explain_storage.sh
+++ zfsmanage/explain_storage/explain_storage.sh
@@ -2,7 +2,7 @@
 #set -x
 
 sepline () {
-	echo "$1-------------------------------------- "
+	echo "$1--------------------------------------  $*"
 }
 tmpd=`mktemp -dt explainstorage`
 export tmpd=$tmpd
@@ -16,30 +16,44 @@ gmultipath status >> $tmpd/infiles/glabel.out
 sesutil map > $tmpd/infiles/ses.out
 zpool status > $tmpd/infiles/zpool.out
 
-sepline ses
-cat $tmpd/infiles/ses.out | ./normalize_ses.nawk | tee $tmpd/tempfiles/ses.normal 
-wc -l $tmpd/tempfiles/ses.normal
-sepline pools
-cat $tmpd/infiles/zpool.out | ./normalize_pool.nawk | tee $tmpd/tempfiles/pool.normal
-wc -l $tmpd/tempfiles/pool.normal
-#not required
-#sepline ses_processesd
-#cat $tmpd/tempfiles/ses.normal | ./join_ses.nawk   | tee $tmpd/ses.out
-#wc -l $tmpd/ses.out
-sepline pools
-cat $tmpd/tempfiles/pool.normal | ./join_pool.nawk | tee $tmpd/pool.out
-wc -l $tmpd/pool.out
+do_ses () {
+	sepline ses
+	echo $tmpd/infiles/ses.out
+	cat $tmpd/infiles/ses.out | ./normalize_ses.nawk | tee $tmpd/tempfiles/ses.normal 
+	wc -l $tmpd/tempfiles/ses.normal
+}
+do_ses
+
+do_pools () {
+	sepline pools
+	echo $tmpd/infiles/zpool.out
+	cat $tmpd/infiles/zpool.out | ./normalize_pool.nawk | tee $tmpd/tempfiles/pool.normal
+	wc -l $tmpd/tempfiles/pool.normal
+	sepline joinedpools
+	cat $tmpd/tempfiles/pool.normal | ./join_pool.nawk | tee $tmpd/pool.out
+	wc -l $tmpd/pool.out
+}
+do_pools
 
 
-sepline smart
-disks=$(ls /dev/*da* )
-for dev in $disks; do
-	echo -n " $dev "
-	echo  "$dev " >> $tmpd/smart.raw
-	smartctl -a $dev  >> $tmpd/smart.raw
-done
-cat $tmpd/smart.raw | ./normalize_smart.nawk   | tee  $tmpd/smart.out
-wc -l $tmpd/smart.out
+do_smart () {
+	sepline smart
+	disks=$( geom disk status | sed -E 's/^ +//' |  cut -w -f1; cd /dev/ ; ls nvme* )
+	for dev in $disks; do
+		echo -n "."
+		printf "\b" 
+		echo  "xpln-start-of /dev/$dev  " >> $tmpd/smart.raw
+		smartctl -a /dev/$dev  >> $tmpd/smart.raw
+		printf "\b" 
+		echo  "xpln-end-of /dev/$dev  " >> $tmpd/smart.raw
+	done
+	echo "smart  collection done"
+	echo $tmpd/smart.raw
+	cat $tmpd/smart.raw | ./normalize_smart.nawk   | tee  $tmpd/smart.out
+	wc -l $tmpd/smart.out
+}
+
+do_smart
 
 ## now create a reverse map from disks, mpaths back to pools
 # this will show unused disks
