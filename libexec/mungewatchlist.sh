9ec505de83dcbb9889d220e839b904844f4bb226
ash <ash@aeria.net>
2021-04-08 17:37:49 -0400

    csv munger prety

--- libexec/mungewatchlist.sh
+++ libexec/mungewatchlist.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x 
-cat $1 | awk '// { FS=","; if ($6> 0.01) {printf ("%3.1f\t%s\t%s\t%s\n",  $6/$5,  $6 , $5, $1); }}' | sort -rn
+cat $1 | awk '// { FS=","; if ($6> 0.01) {printf ("%3.1f\t%4.2f\t%4.2f\t%s\n",  $6/$5,  $6 , $5, $1); }}' | sort -rn
