ca5524b525cef10245d886e3fe394111c2f75d66
xpi <you@example.com>
2021-04-15 18:32:32 +0000

    moresalt

--- howto/linux/salty.sh
+++ howto/linux/salty.sh
@@ -2,3 +2,10 @@
 sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
 # Create apt sources list file
 echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee /etc/apt/sources.list.d/salt.list
+sudo apt-get update
+sudo apt-get install salt-master
+sudo apt-get install salt-minion
+sudo apt-get install salt-ssh
+sudo apt-get install salt-syndic
+sudo apt-get install salt-cloud
+sudo apt-get install salt-api
