8f45d774e824b25144937139e3b30c968473c66b
ash <ashfixit@gmail.com>
2022-01-26 15:30:11 -0500

    >=0

--- libexec/histo.c
+++ libexec/histo.c
@@ -5,15 +5,14 @@ int main ( int argc, char ** argv ){
 unsigned int histo[256];
 int ic = 66;
 
-explicit_bzero (histo, 256); 
 
 for (int i=0; i<256 ; i++ ){ histo[i]=0;}
 	do {
 	ic = getchar();
-	if ( ic > 0 )
+	if ( ic >= 0 )
 		histo[ic] ++; 
 	//putchar (ic);
-	} while ( ic > 0);
+	} while ( ic >= 0);
 
 for (unsigned int i=0; i<256; i++) {
 	if (histo[i]> 0) { 
