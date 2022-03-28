0d3f9e7b774873b9fd9e15f7090f784cf376836f
ash <ash@aeria.net>
2022-03-28 11:11:58 -0400

    ++bubblewrap bulkrename

--- libexec/bulkrename.sh
+++ libexec/bulkrename.sh
@@ -3,10 +3,16 @@
 srcpat=$1
 destpat=$2
 
+if [ 2 -ne  $# ]; then   
+	echo $# args: $*
+	echo usage: bulkrename srcpatern dstpatern
+	exit 3
+fi 
+
 echo renaming $1 to $2
 srcnames=` ls  |  grep "$srcpat"`
 for i  in $srcnames; do
-	echo  mv $i `echo $i | sed -e "s/$srcpat/$destpat/"`
+	echo  \#mv $i `echo $i | sed -e "s/$srcpat/$destpat/"`
 done
 echo ok?
 read ok
