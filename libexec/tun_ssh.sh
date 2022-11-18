842e0adffe8df677be315f3c668a52c83f88eb6e
ash <ash@aeria.net>
2022-11-17 19:00:34 -0500

    update tunnelbetr

--- libexec/tun_ssh.sh
+++ libexec/tun_ssh.sh
@@ -6,7 +6,7 @@
 
 hname=`hostname`
 unum=`hostname | od | sum | cut -f1 -d" "`
-tunneltarget='tunnelclient@aeria.net'
+tunneltarget=${1:-'tunnelclient@aeria.net'}
 echo $unum
 choix=`dc -e "$unum 65553 % p"`
 echo "going to fwd: "
@@ -14,7 +14,7 @@ echo $choix
 while true; do
     now=`date +'%s'`
 	ssh -v  $tunneltarget  "mkdir -p tmp/tunneldb &&\
-         echo '$now	$choix' >> tmp/tunneldb/\\$hname "
+         echo '$now	$choix	$SSH_CLIENT' >> tmp/tunneldb/\\$hname "
 	ssh -v -N -R $choix:localhost:22 $tunneltarget
 	sleep 600
 done
