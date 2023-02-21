1e1bbc983eeb7c9e193c872d2a52ab3691fa9fda
ash <ash@aeria.net>
2023-02-21 08:50:34 -0500

    mvg works now

--- libexec/openmvg_simplepipeline.sh
+++ libexec/openmvg_simplepipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -xe 
+#cribbed from https://github.com/openMVG/openMVG/blob/develop/src/software/SfM/SfM_GlobalPipeline.py.in
 imgdir=$1
 outdir=$2
 
@@ -7,23 +8,17 @@ featd="$outdir/feature_hop"
 recond="$outdir/reconstruction_hop"
 alld="$matchd $featd $recond"
 camdb=/usr/local/share/openMVG/sensor_width_camera_database.txt
+mkdir -p $outdir
 mkdir -p $alld
 
 openMVG_main_SfMInit_ImageListing -i $imgdir -o $matchd -d $camdb #generates sfm_data.json
 sfmdata="$matchd/sfm_data.json"
-ls -l $sfmdata
 openMVG_main_ComputeFeatures -i $sfmdata -o $matchd -m SIFT
-ls -lrt $featd
-openMVG_main_ComputeMatches -i $sfmdata -o $matchd
-ls -lrt $matchd
-openMVG_main_IncrementalSfM -i $sfmdata -m $matchd -o $recond
-sfmdatabin="$recond/sfm_data.bin"
-ls -l $sfmdatabin
-openMVG_main_ComputeSfM_DataColor -i $sfmdatabin -o $recond/colorized.ply
-colrply="$recond/colorized.ply"
-ls -l $colrply
-openMVG_main_ComputeStructureFromKnownPoses -i $sfmdatabin -m $matchd -f $matchd/matches.f.bin -o $recond/robust.bin
-ls -l $recond/robust.bin
-openMVG_main_ComputeSfM_DataColor -i $recond/robust.bin -o $recond/robust_colorized.ply
+openMVG_main_PairGenerator -i $sfmdata -o ${matchd}/pairs.bin
+openMVG_main_ComputeMatches -i $sfmdata -p ${matchd}/pairs.bin -o $matchd/matches.putative.bin
+openMVG_main_GeometricFilter -i $sfmdata  -m $matchd/matches.putative.bin -g e -o $matchd/matches.e.bin
+openMVG_main_SfM --sfm_engine GLOBAL --input_file  $sfmdata --match_file ${matchd}/matches.e.bin --output_dir $recond
+openMVG_main_ComputeStructureFromKnownPoses -i ${recond}/sfm_data.bin -m $matchd -f $matchd/matches.e.bin -o $recond/robust.bin
+openMVG_main_ComputeSfM_DataColor -i ${recond}/sfm_data.bin -o ${recond}/colorized.ply
 
 
