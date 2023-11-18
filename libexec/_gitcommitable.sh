2e9a9f498752ba18fcc198be9c6b328ac12ae6c0
agokhale <ash@aeria.net>
2023-11-18 16:30:33 -0500

    betr git targets for add

--- libexec/_gitcommitable.sh
+++ libexec/_gitcommitable.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-git status | awk '/not staged/ {lat=1;} /Untracked/ {lat=0} /modi/{if (lat) print ($2)}'
+git status | awk '/not staged/ {lat=1;} /Untracked/ {lat=0} /modi/{if (lat) print ($2)} /new file:/ { print($3);} '
