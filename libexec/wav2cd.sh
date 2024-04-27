f79cb463d8fe4630052b5e878b11e461af81628c
agokhale <ash@aeria.net>
2024-04-27 06:52:42 -0400

    += rate converter, softclip, cdrecord gak

--- libexec/wav2cd.sh
+++ libexec/wav2cd.sh
@@ -1,4 +1,26 @@
 #!/bin/sh -ex
-find . -name "*.wav" -print0 | xargs -0 -n1 -I% sox "%" "%".cdr
-echo cdrecord dev=3,0,0 -v -audio -dao *.cdr
+outfmt="cdr"
+soxflags="-V4" # verbose
+outrate="44100"
+cdrom_scsiid="9,0,0"
+
+# -v sets vry high SR conversion
+# vol 0.98 should eat hard clips - mostly :/
+
+find . -name "*.wav" -print0 | \
+  xargs -0 -n1 -I% \
+    sox  $soxflags  "%" "%".$outfmt  \
+      vol 0.98 \
+      rate -v $outrate
+
+#cdrom=` cdrecord -scanbus | grep CD-ROM | cut -w  -f2 ` ## needs a bunch of  root perms for the pass, xpt drivers
+# incomplete devfs
+#own     cd0     root:operator
+#perm    cd0     0660
+#operator should be able to cdrecord -scanbus
+#own     xpt0    root:operator
+#perm    xpt0    0660
+
+echo cdrecord dev=$cdrom_scsiid -v -audio -dao \*.cdr | tee  burnfile.sh
+chmod 700 burnfile.sh
 
