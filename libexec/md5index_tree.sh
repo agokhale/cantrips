8439f69ea2c5bee773147444a423757b5a89319a
ash <ash@aeria.net>
2024-12-28 13:02:19 -0500

    xsplain

--- libexec/md5index_tree.sh
+++ libexec/md5index_tree.sh
@@ -5,7 +5,9 @@ usage(){
 
 
 #find position depentant  so print0 is last
-find -s .   -print0 \
+# -x don't cross fs mounts
+# -s lex ordering
+find -x -s .   -print0 \
 	| xargs -0 -n1 -I%   \
 		md5sum % >> index.md5 
 
