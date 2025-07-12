a277fc41288f023f207692dee78550bd4f1b9376
agokhale <ash@aeria.net>
2025-07-12 02:09:32 -0400

    tokenize rate/dev

--- howto/audio/ossdvd_dm3.sh
+++ howto/audio/ossdvd_dm3.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -xe
 # for whatever reason this needs to be run twice, or the audio is distrorted un der 14.2
+pcmdev="9"
+rate="96000"
 
-sysctl dev.pcm.3.rec.vchanrate=96000
-sysctl dev.pcm.3.play.vchanrate=96000
+sysctl dev.pcm.$pcmdev.rec.vchanrate=$rate
+sysctl dev.pcm.$pcmdev.play.vchanrate=$rate
 
 #speculative
-	sysctl hw.snd.default_unit=3
+#	sysctl hw.snd.default_unit=$pcmde
 	sysctl hw.snd.verbose=4
 	kldload mac_priority.ko || true
 
@@ -19,9 +21,9 @@ virtual_oss \
                    -S \
                    -Q 0 \
                    -i 8 \
-                   -C 18 -c 18 -r 96000 -b 32 \
+                   -C 18 -c 18 -r $rate -b 32 \
 		-s 8ms \
-		-f /dev/dsp3 \
+		-f /dev/dsp$pcmdev \
                 -a 0 -c 2 -m 0,16,1,17 \
 		-d dsp \
                 -a 0 -b 32 -c 16 -m 0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15 \
