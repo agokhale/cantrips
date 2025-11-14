51014d63bb1184d63aaad2aa6f1fcb926b06544a
agokhale <ash@aeria.net>
2025-11-14 15:43:11 -0500

    install moar

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -xe
-
-ln -s ${HOME}/cantrips/cshrc ${HOME}/.cshrc
-ln -s ${HOME}/cantrips/env/screenrc ${HOME}/.screenrc
+#!/bin/sh -x
+ln -sf ${HOME}/cantrips/cshrc ${HOME}/.cshrc
+ln -sf ${HOME}/cantrips/env/screenrc ${HOME}/.screenrc
+ln -sf ${HOME}/cantrips/env/nexrc ${HOME}/.exrc
