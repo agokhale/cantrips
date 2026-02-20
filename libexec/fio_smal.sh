89325785e636cf09b7c6593da62c92b0041f16a3
agokhale <1426679+agokhale@users.noreply.github.com>
2026-02-20 08:50:48 -0500

    param path

--- libexec/fio_smal.sh
+++ libexec/fio_smal.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+set -e 
+set -x
 NAME=${NAME:=$1}
 NAME=${NAME:=default}
+tgpath=$2
 BLOCKSIZES="16k"
 LOOPS="01 "
 JOBS="2"
@@ -10,18 +13,18 @@ RUNTIME=120
 echo "Starting run..."
 mkdir ./${NAME}
 for b in $BLOCKSIZES; do
-	rm -rf /persist/fio
-	mkdir -p /persist/fio/$b
+	rm -rf ${tgpath}
+	mkdir -p ${tgpath}
 	time sync
 	# Do Allocation
-	fio --directory=/persist/fio/$b --name=bench_$b --rw=write --bs=$b --iodepth=16 --size=${SIZE}G --end_fsync=1 --ioengine=psync --group_reporting --fallocate=none --io_size=${SIZE}G
+	fio --directory=${tgpath} --name=bench_$b --rw=write --bs=$b --iodepth=16 --size=${SIZE}G --end_fsync=1 --ioengine=psync --group_reporting --fallocate=none --io_size=${SIZE}G
 	time sync
 	time sync
 	# Do runs
 	for j in $JOBS; do
 		for r in $LOOPS; do
-                        fio --directory=/persist/fio/$b --name=bench_$b --rw=randread --bs=$b --numjobs=${j} --iodepth=16 --size=${SIZE}G --end_fsync=1 --ioengine=psync --group_reporting --fallocate=none --runtime=$RUNTIME --time_based --output-format=json >> ./${NAME}/read_${b}_${j}_${r}.json
-                        fio --directory=/persist/fio/$b --name=bench_$b --rw=randwrite --bs=$b --numjobs=${j} --iodepth=16 --size=${SIZE}G --end_fsync=1 --ioengine=psync --group_reporting --fallocate=none --runtime=$RUNTIME --time_based --output-format=json >> ./${NAME}/write_${b}_${j}_${r}.json
+                        fio --directory=${tgpath} --name=bench_$b --rw=randread --bs=$b --numjobs=${j} --iodepth=16 --size=${SIZE}G --end_fsync=1 --ioengine=psync --group_reporting --fallocate=none --runtime=$RUNTIME --time_based --output-format=json >> ./${NAME}/read_${b}_${j}_${r}.json
+                        fio --directory=${tgpath} --name=bench_$b --rw=randwrite --bs=$b --numjobs=${j} --iodepth=16 --size=${SIZE}G --end_fsync=1 --ioengine=psync --group_reporting --fallocate=none --runtime=$RUNTIME --time_based --output-format=json >> ./${NAME}/write_${b}_${j}_${r}.json
                         time sync
                 done
                 jq '.jobs[0].read.bw' ./${NAME}/read_${b}_${j}_*.json > ./${NAME}/read_bw_summary_${b}_${j}.txt
